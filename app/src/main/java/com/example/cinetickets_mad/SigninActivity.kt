2366c09794dc103b8858a0a4c3a69d92885aff09
Buwaneka Wijesinghe <128914545+Buwaneka99@users.noreply.github.com>
2024-08-18 20:43:26 +0530

    changes

---
+++ app/src/main/java/com/example/cinetickets_mad/SigninActivity.kt
@@ -0,0 +1,30 @@
+package com.example.cinetickets_mad
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val signInBtn: Button = findViewById(R.id.signInBtn)
+
+        signInBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
