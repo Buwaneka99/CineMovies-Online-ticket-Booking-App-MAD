62337b71b75b9eee4615b8b53c8280bb05223975
Buwaneka Wijesinghe <128914545+Buwaneka99@users.noreply.github.com>
2024-08-18 21:36:08 +0530

    changes

--- app/src/main/java/com/example/cinetickets_mad/ProfileActivity.kt
+++ app/src/main/java/com/example/cinetickets_mad/ProfileActivity.kt
@@ -1,12 +1,17 @@
 package com.example.cinetickets_mad
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ProfileActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,22 @@ class ProfileActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backBtn: ImageView = findViewById(R.id.backBtn)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val logOutBtn: Button = findViewById(R.id.logOutButton)
+
+        logOutBtn.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
